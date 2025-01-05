76b2b93f61183b26bb8e64e618b54bbfb72f2f6b
junlin-surreal <junlin.pan@surreal.interactive.com>
2025-01-05 17:12:26 +0800

    minor

--- Assets/SVR/Scripts/Core/SVRControllerManager.cs
+++ Assets/SVR/Scripts/Core/SVRControllerManager.cs
@@ -54,10 +54,10 @@ public class SVRControllerManager : MonoBehaviour
             product = "Surreal-Interactive"
         });
         InputSystem.SetDeviceUsage(right_device, UnityEngine.InputSystem.CommonUsages.RightHand);
-        left_device.x_ = 128;
-        left_device.y_ = 128;
-        right_device.x_ = 128;
-        right_device.y_ = 128;
+        left_state.x_ = 128;
+        left_state.y_ = 128;
+        right_state.x_ = 128;
+        right_state.y_ = 128;
 
 #if UNITY_IOS || UNITY_VISIONOS
         svr.SVRInputApi.SVRStart();
